62a09e6dae6541b72a08907fb362302cde5d3edb
Chris Matlak <chrismatlak3.14@gmail.com>
2020-12-03 22:01:47 -0500

    Implement core cipher concepts
    Additionaly,
    * Add tests
    * Implement Dvorak and Workman layouts

--- cipher.py
+++ cipher.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3.8
+
+import click
+import os
+from typing import *
+
 """
 Program execution
 1. Main loads text and options
@@ -16,3 +22,247 @@ Validation checks
 * (Characters in the input that are not in the layout are skipped)
 * Optional: ensure that each letter appears once in the layout (no skipped letters)
 """
+
+
+def p(content):
+    click.echo(content, err=False)
+
+
+def e(content):
+    click.echo(content, err=True)
+
+
+class Keycap:
+    def __init__(self, display: chr):
+        self.blank: bool = False if display.strip() else True
+        self.name: chr = display if not self.blank else " "
+        self.right: Keycap
+        self.left: Keycap
+        self.up: Keycap
+        self.down: Keycap
+        self.surround: List[Keycap] = []  # these ones are reversible
+
+    def __repr__(self) -> str:
+        return self.name if not self.blank else "∞"
+
+    @property
+    def NE(self) -> "Keycap":
+        return self.up.right
+
+    @property
+    def NW(self) -> "Keycap":
+        return self.up.left
+
+    @property
+    def SW(self) -> "Keycap":
+        return self.down.left
+
+    @property
+    def SE(self) -> "Keycap":
+        return self.down.right
+
+    @property
+    def raw_surround(self) -> "List[Keycap]":
+        """
+        Returns the 8 characters surrounding the letter starting with the one
+        to the right and proceeding counterclockwise
+        """
+        return [
+            self.right,
+            self.NE,
+            self.up,
+            self.NW,
+            self.left,
+            self.SW,
+            self.down,
+            self.SE,
+        ]
+
+    @property
+    def surround_str(self) -> str:
+        return "".join([str(s) for s in self.surround])
+
+    @property
+    def raw_surround_str(self) -> str:
+        return "".join(str(s) for s in self.raw_surround)
+
+    def bake_surround(self) -> int:
+        """
+        Filters out the blank and non-reversible keys from raw_surround
+        and then saves it to self.surround
+
+        Returns the number of non-reverisble keys that were thrown out
+        (not counting blanks)
+        """
+        self.surround = [
+            k for k in self.raw_surround if self in k.raw_surround and not k.blank
+        ]
+        return (
+            len(self.raw_surround)
+            - len(self.surround)
+            - len([k for k in self.raw_surround if k.blank])
+        )
+
+
+class Cipher:
+    def __init__(
+        self, layout: str = "QWERTY", alphabet_check: int = 26, reverse: bool = None
+    ):
+        layout = layout.lower().strip()
+        if not layout:
+            layout = "qwerty"
+        if reverse is None and layout == "dvorak":
+            reverse = True  # the symbol keys are on the left on Dvorak
+
+        # Load the layout
+        self.alphabet: str = ""
+        self.letter_index: Dict[chr, Keycap] = {}
+        self.grid: List[List[Keycap]] = []
+        self.height: int = 0
+        for row in open(os.path.join(os.path.dirname(__file__), f"layouts/{layout}")):
+            row = row.lower().strip()
+            self.alphabet += row
+            self.grid.append([])
+            for chr in row:
+                c = Keycap(chr)
+                self.letter_index[chr] = c
+                self.grid[self.height].append(c)
+            self.height += 1
+        self.row_lengths: List[int] = [len(r) for r in self.grid]
+        max_len = max(self.row_lengths)
+
+        # Validity checks
+        assert len(self.alphabet) == len(
+            self.letter_index.keys()
+        ), f"Layout has at least one repeated letter"
+        if alphabet_check > 0:
+            dif: int = len(self.alphabet) - alphabet_check
+            if dif > 0:
+                e(f"Layout contains {dif} more letters than the alphabet, continuing.")
+            assert (
+                dif >= 0
+            ), f"Layout is missing {-dif} letter{'s' if dif < -1 else ''} of the alphabet"
+
+        # Arrange the keycaps to a grid
+        for r in range(self.height):
+            if reverse:
+                self.grid[r].reverse()
+            for c in range(1, l := self.row_lengths[r]):
+                cap: Keycap = self.grid[r][c]
+                left: Keycap = self.grid[r][c - 1]
+                right: Keycap = self.grid[r][(c + 1) % l]
+
+                cap.left = left
+                cap.right = right
+                left.right = cap
+                right.left = cap
+
+            # blank keycaps for padding
+            # ALWAYS ON THE RIGHT
+            for x in range(l, max_len):
+                k = Keycap("")
+                k.left = self.grid[r][x - 1]
+                if k.left.blank:
+                    k.left.right = k
+                if x == max_len - 1:
+                    k.right = self.grid[r][0]
+                self.grid[r].append(k)
+            if reverse:
+                self.grid[r].reverse()
+
+        # assign top and bottom keys
+        for r in range(self.height - 1):
+            up = r - 1
+            down = r + 1
+            for c in range(0, max_len):
+                k = self.grid[r][c]
+                u = self.grid[up][c]
+                d = self.grid[down][c]
+
+                k.up = u
+                k.down = d
+                u.down = k
+                d.up = k
+
+        # bake in the reversible keys
+        [(key, key.bake_surround(), key.surround) for key in self.letter_index.values()]
+
+    def surround(self, letter: chr, full: bool = False) -> List[chr]:
+        """
+        Returns the 8 characters surrounding the letter starting with the one
+        to the right and proceeding counterclockwise
+
+        Param `full` controls whether or not to display the blank keys
+        """
+        return [
+            k.name for k in self.letter_index[letter].surround if not k.blank or full
+        ]
+
+
+nav_guide = ("❇", ["➡️", "↗️", "⬆️", "↖️", "⬅️", "↙️", "⬇️", "↘️"])
+
+
+def letter_test_grid(c: Cipher, l: chr) -> str:
+    """
+    Generates a grid of letters that either appear in the surround of l or have
+    l in their surrounding.  Exclamation points are appended to lines for
+    letters that contain l in their surroundings but do not surround l.
+    Question marks added to lines for letters in the surrounding of l that do
+    not also contain l in their surrounding.
+
+    """
+    o: List[str] = []
+    ring: List[str] = c.surround(l, True)
+    o.append(str((l, ring)))
+    o.append(str(nav_guide) + "🔛")
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        s = c.surround(letter)
+        if l in s or letter in ring:
+            o.append(
+                str((letter, c.surround(letter, True)))
+                + f"\t{'‼' if letter not in ring else ''}"
+                + f"\t{'❔' if l not in s else ''}"
+            )
+    o.append(str(nav_guide) + "🔛")
+    o.append(str((l, ring)))
+    return "\n".join(o)
+
+
+@click.command()
+@click.argument("text", nargs=-1)
+@click.option(
+    "-k",
+    "--layout",
+    default="QWERTY",
+    type=click.STRING,
+    help="""
+        The layout of the keyboard you use for the cipher.
+
+        Technically, this is the name of a file in the layouts directory.
+
+        The -k shortname stands for keyboard.
+        """,
+)
+@click.option(
+    "-f",
+    "--from-file",
+    "file",
+    type=click.File(),
+    help=""" 
+        Ignore the TEXT in favor of sharking a file one line at a time.  A
+        value of '-' will shark stdin.
+        """,
+)
+def shark(text: str, layout: str = "QWERTY"):
+    """
+    Runs TEXT through the shark cipher
+
+    TEXT can be either the plaintext or the ciphertext.  Due to the
+    eccentricities of human keyboard layouts, this cipher is not fully
+    reversible.
+    """
+    c = Cipher(layout)
+
+
+if __name__ == "__main__":
+    shark()
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.8
 
 import click
 from typing import *
@@ -13,19 +13,113 @@ character is in the 8 possibilities presented for each character of ciphertext.
 """
 
 
-def test(phrase: str) -> bool:
-    phrase = phrase.lower().strip()
-    pass  # TODO write the test
+def phrase_check(phrase: str) -> str:
+    """
+    Checks that the phrase exists and is in the lower case
+
+    If it does not exist, use a default phrase
+    """
+    ph = phrase.lower().strip()
+    return ph if ph else "the quick brown fox jumped over the lazy god"
+
+
+def reverse(layout: str, phrase: str) -> bool:
+    """
+    Encrypts a phrase randomly and then checks that the original phrase exists
+    among the possible decryptions
+    """
+    phrase = phrase_check(phrase)
+    return True  # TODO
+
+
+def association_check(layout: str) -> bool:
+    """
+    Checks that each letter is contained in the surrounding of each letter that
+    surrounds it.
+    """
+    c = Cipher(layout)
+    fail_count: int = 0
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        if all(letter in q.surround_str for q in c.letter_index[letter].surround):
+            pass
+        else:
+            e(f"'{letter}' lacks proper linkbacks under {layout}")
+            e(letter_test_grid(c, letter))
+            fail_count += 1
+    if fail_count:
+        e(f"{fail_count} mismatches in {layout}")
+        return False
+    return True
+
+
+def test_invalid_repeat() -> bool:
+    """Ensure invalid layouts are not accepted"""
+    try:
+        c = Cipher("test-invalid")
+    except AssertionError:
+        return True  # that's probably what threw the error
+    return False  # no error thrown on bad layout file => FAIL
+
+
+def test_catch_missing() -> bool:
+    """Ensure that layouts with missing letters are caught"""
+    try:
+        c = Cipher("test-missing")
+    except AssertionError:
+        return True  # it's probably the assertion, check stderr
+    return False  # it didn't catch the missing letters
+
+
+def test_ignore_missing() -> bool:
+    """Do not raise an error if there are more letters than expected"""
+    try:
+        c = Cipher("test-missing", 4)
+    except AssertionError:
+        return False
+    return True
+
+
+def run_tests(phrase: str) -> List[bool]:
+    phrase = phrase_check(phrase)
+    p(f"Test layout validation")
+    p(f"Expect something in stderr")
+    results: List[bool] = [
+        test_catch_missing(),
+        test_invalid_repeat(),
+        test_ignore_missing(),
+    ]
+    for layout in [
+        "QWERTY",
+        "Colemak",
+        "Workman",
+        "Dvorak",
+        "test-colemak",
+        "test-alphabet",
+    ]:
+        p(f"Testing in {layout}…")
+        results.extend(
+            [
+                association_check(layout),
+                reverse(layout, phrase),
+            ]
+        )
+    return results
 
 
 @click.command()
 @click.argument("ph", nargs=-1)
 def cli_test(ph: List[str]) -> bool:
+    """
+    Runs the tests.
+
+    PH is an optional argument for a custom phrase to test.
+    """
     phrase: str = " ".join(ph) if ph else "Jackdaws love my big sphinx of quartz."
-    if test(phrase):
-        print("Test passed!")
+    # p(phrase)
+    if all(tsts := run_tests(phrase)):
+        p("Tests passed!")
     else:
-        print("Test failure")
+        e(f"{len([t for t in tsts if t is False])} out of {len(tsts)} tests failed!")
 
 
 if __name__ == "__main__":
